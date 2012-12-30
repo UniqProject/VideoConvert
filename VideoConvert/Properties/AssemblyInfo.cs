a5cea058dc00599f9f6c01b848faefd15ad6987b
Juergen Tem <j.tem@gmx.de>
2012-12-30 16:03:29 +0100

    update changelog

--- VideoConvert/Properties/AssemblyInfo.cs
+++ VideoConvert/Properties/AssemblyInfo.cs
@@ -70,5 +70,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.21.714.1746")]
-[assembly: AssemblyFileVersion("0.21.714.1746")]
+[assembly: AssemblyVersion("0.21.716.1152")]
+[assembly: AssemblyFileVersion("0.21.716.1152")]
