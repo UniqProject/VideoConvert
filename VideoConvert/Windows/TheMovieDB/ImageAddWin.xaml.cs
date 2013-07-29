89d736bb11c0776c639cd89ae0258bf08488d9c3
Juergen Tem <j.tem@gmx.de>
2013-07-29 10:16:59 +0200

    new window for adding images

---
+++ VideoConvert/Windows/TheMovieDB/ImageAddWin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace VideoConvert.Windows.TheMovieDB
+{
+    /// <summary>
+    /// Interaktionslogik für ImageAddWin.xaml
+    /// </summary>
+    public partial class ImageAddWin
+    {
+
+        public string ResultPreview = string.Empty;
+        public string ResultImage = string.Empty;
+
+        public ImageAddWin()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultImage = ImageUrl.Text;
+            ResultPreview = PreviewUrl.Text;
+
+            DialogResult = true;
+        }
+    }
+}
