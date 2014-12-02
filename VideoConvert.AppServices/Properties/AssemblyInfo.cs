bb103c0c820bcfe17c7ea14d46922c11d5f494b7
Juergen Tem <j.tem@gmx.de>
2014-12-02 13:37:10 +0100

    version info

--- VideoConvert.AppServices/Properties/AssemblyInfo.cs
+++ VideoConvert.AppServices/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.50.0420.347")]
-[assembly: AssemblyFileVersion("0.50.0420.347")]
+[assembly: AssemblyVersion("0.50.1202.357")]
+[assembly: AssemblyFileVersion("0.50.1202.357")]
